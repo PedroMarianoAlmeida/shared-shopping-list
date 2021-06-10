7d9a45c6364051eb90caaa1c0846e5b2a1aa963f
Pedro Almeida <59484474+PedroMarianoAlmeida@users.noreply.github.com>
2021-06-10 15:17:16 -0300

    Refactor: Move folder of AppDrawer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,30 +2,16 @@ import type { AppProps } from 'next/app';
 
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Container from '@material-ui/core/Container';
-import { makeStyles } from '@material-ui/core/styles';
 
-import AppDrawer from '../src/components/Wrapper/AppDrawer';
-
-const appBarHeight = '64px';
-const drawerWidth = '45px';
-
-const useStyles = makeStyles({
-  main: {
-    marginTop: appBarHeight,
-  },
-});
+import AppDrawer from '../src/components/AppDrawer';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const classes = useStyles();
-
   return (
     <>
       <CssBaseline />
 
-      <AppDrawer drawerWidth={drawerWidth} appBarHeight={appBarHeight}>
-        <Container className={classes.main}>
-          <Component {...pageProps} />
-        </Container>
+      <AppDrawer>
+        <Component {...pageProps} />
       </AppDrawer>
     </>
   );
